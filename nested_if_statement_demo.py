7e2fd3eb2ef2646e7326a53d6c0b76ea80027474
nisha-amali <amalinisha16@gmail.com>
2025-11-08 16:24:21 +0530

    Add files via upload

---
+++ nested_if_statement_demo.py
@@ -0,0 +1,20 @@
+# Access to the portal
+username = "amalinisha16@gmail.com"
+password = "Nisha10#"
+if username == "amalinisha16@gmail.com":
+    if password == "Nisha10#":
+        print("Access granted.")
+    else:
+        print("Check your password.")
+else:
+    print("Access denied.")
+# Eligible to vote or not
+age = 18
+if age >= 18:
+    ID = False
+    if ID:
+        print("you are eligible to vote.")
+    else:
+        print("You need an ID to vote.")
+else:
+    print("You must be at-least 18 to vote.")
